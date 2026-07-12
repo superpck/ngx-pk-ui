5162fe97313db96627885a23f25dfbba97afe6e6
superpck <superpck@gmail.com>
2026-07-12 18:55:09 +0700

    Bug fix ChangeDetectorRef

--- projects/example/src/app/pages/pk-tabs/pk-tabs-page.ts
+++ projects/example/src/app/pages/pk-tabs/pk-tabs-page.ts
@@ -1,10 +1,16 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { PkTabsModule } from 'ngx-pk-ui';
 
 @Component({
   selector: 'app-pk-tabs-page',
-  imports: [PkTabsModule],
+  imports: [
+    PkTabsModule,
+    FormsModule
+  ],
   changeDetection: ChangeDetectionStrategy.Eager,
   templateUrl: './pk-tabs-page.html',
 })
-export class PkTabsPage {}
+export class PkTabsPage {
+  activeTab = signal<string>('profile');
+}
